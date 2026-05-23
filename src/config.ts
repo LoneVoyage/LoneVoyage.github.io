f0c56fb690d30c4e9518d7f01a3ef4819df73a05
LoneVoyage <lonevoyage@foxmail.com>
2026-05-23 08:41:47 +0800

    chore: edit config

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ import type {
 import type { FriendLink } from "./types/friend"
 
 export const siteConfig: SiteConfig = {
-    title: "Momo",
+    title: "LoneVoyage",
     subTitle: "Blog",
 
     favicon: "/favicon/favicon.ico", // Path of the favicon, relative to the /public directory
@@ -21,37 +21,37 @@ export const siteConfig: SiteConfig = {
         enable: true // Whether to enable blog navigation in the blog footer
     },
     comments: {
-        enable: true, // Whether to enable comments
+        enable: false, // Whether to enable comments
         platform: "default", // Comment platform, set "default" to use Momo-backend, also supports "twikoo"
-        backendUrl: "https://api-momo.motues.top" // Backend URL for comments
+        backendUrl: "https://www.example.com" // Backend URL for comments
     },
     theme: {
-        AOS: true, // Whether to enable AOS (Animate On Scroll) for animations
+        AOS: false, // Whether to enable AOS (Animate On Scroll) for animations
         LQIP: true, // Whether to enable LQIP (Low-Quality Image Placeholder) for image placeholders
         PhotoSwipe: true // Whether to enable PhotoSwipe for image viewer
     }
 }
 
 export const profileConfig: ProfileConfig = {
-    avatar: "assets/Motues.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-    name: "Motues",
-    description: "Life is colorful!",
-    indexPage: "https://www.motues.top",
-    startYear: 2024,
+    avatar: "assets/LoneVoyage.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    name: "LoneVoyage",
+    description: "Long way to go",
+    indexPage: "https://lonevoyage.github.io",
+    startYear: 2026,
 }
 
 export const licenseConfig: LicenseConfig = {
-	enable: true,
+	enable: false,
 	name: "CC BY-NC-SA 4.0",
 	url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
 };
 
 export const friendLinkConfig: FriendLink[] = [
     {
-        name: 'Motues',
-        avatar: 'https://www.motues.top/avatar.jpg',
-        url: 'https://www.motues.top',
-        description: 'Like River!'
+        name: 'LoneVoyage',
+        avatar: 'https://lonevoyage.github.io/avatar.jpg',
+        url: 'https://lonevoyage.github.io',
+        description: 'Long way to go'
     },
     {
         name: 'Astro',
